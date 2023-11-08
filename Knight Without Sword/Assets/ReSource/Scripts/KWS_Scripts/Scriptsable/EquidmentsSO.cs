3722752e813ee0778ee6d4d9a219b01fcec68fe1
toan0812 <80931103+toan0812@users.noreply.github.com>
2023-11-08 14:37:53 +0700

    Update Script

--- Knight Without Sword/Assets/ReSource/Scripts/KWS_Scripts/Scriptsable/EquidmentsSO.cs
+++ Knight Without Sword/Assets/ReSource/Scripts/KWS_Scripts/Scriptsable/EquidmentsSO.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [CreateAssetMenu()]
 public class EquidmentsSO : ItemSO
 {
+    public int goldPrice;
+    public int gemPrice;
     private void Awake()
     {
         type = ItemsType.Equipment;
