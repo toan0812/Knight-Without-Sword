d4c4b79497aa80ba94a74af1de788317946c459c
toan0812 <80931103+toan0812@users.noreply.github.com>
2023-11-08 15:16:19 +0700

    Update buy Item

--- Knight Without Sword/Assets/ReSource/Scripts/KWS_Scripts/UI/ButtonUI.cs
+++ Knight Without Sword/Assets/ReSource/Scripts/KWS_Scripts/UI/ButtonUI.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 public class ButtonUI : MonoBehaviour
 {
+    [Header("Change Color")]
     [SerializeField] bool isChangingColor;
     [SerializeField] ShopUI shopUI;
+    [Header("ItemSO")]
     [SerializeField] EquidmentsSO equidmentsSO;
     [SerializeField] WeaponItemsSO weaponItemsSO;
+    [Header("Text")]
+    [SerializeField] private Text buyGoldText;
+    [SerializeField] private Text buyGemText;   
+    [Header("Buttons")]
+    [SerializeField] private Button buyGoldButton;
+    [SerializeField] private Button buyGemButton;
+
+    private PlayerData playerData= new PlayerData();
     void Start()
     {
         shopUI = GameObject.FindAnyObjectByType<ShopUI>();
@@ -17,6 +28,40 @@ public class ButtonUI : MonoBehaviour
         {
             shopUI.ShowInforButtonUI();
         });
+        buyGoldButton.onClick.AddListener(() =>
+        {
+            BuyItem(playerData.gold, Int32.Parse(buyGoldText.text));
+        });
+        buyGemButton.onClick.AddListener(() =>
+        {
+            BuyItem(playerData.gem, Int32.Parse(buyGemText.text));
+        });
+
+    }
+
+    public void BuyItem(int budget, int price)
+    {
+        if(budget>=price)
+        {
+            budget -= price;
+        }
+       else
+        {
+            Debug.Log("can not buy item");
+        }
+    }
+    public void SetPriceTextForItem()
+    {
+        if(equidmentsSO!= null)
+        {
+            buyGoldText.text = equidmentsSO.goldPrice.ToString();
+            buyGemText.text = equidmentsSO.gemPrice.ToString();
+        }
+        if(weaponItemsSO != null)
+        {
+            buyGoldText.text = weaponItemsSO.goldPrice.ToString();
+            buyGemText.text = weaponItemsSO.gemPrice.ToString();
+        }
     }
     public void SetEquidmentsSO(EquidmentsSO equidmentsSO)
     {
--- Knight Without Sword/Assets/ReSource/Scripts/KWS_Scripts/UI/ShopUI.cs
+++ Knight Without Sword/Assets/ReSource/Scripts/KWS_Scripts/UI/ShopUI.cs
@@ -89,6 +89,7 @@ public class ShopUI : MonoBehaviour
                 var gunItemUI = Instantiate(gunItemsUI, gunItemsViewPort.transform);
                 gunItemUI.transform.GetChild(0).GetComponent<Image>().sprite = WeaponManager.Instance.GetWeaponItemsFromDB()[i].prefabImage;
                 gunItemUI.GetComponent<ButtonUI>().SetWeaponSO(WeaponManager.Instance.GetWeaponItemsFromDB()[i]);
+                gunItemUI.GetComponent<ButtonUI>().SetPriceTextForItem();
                 gunItemUI.SetActive(true);
                 holder.Add(gunItemUI);
             }
@@ -108,6 +109,7 @@ public class ShopUI : MonoBehaviour
                 var ItemUI = Instantiate(equidmentItemsUI, equidmentItemsViewPort.transform);
                 ItemUI.transform.GetChild(0).GetComponent<Image>().sprite = ItemsManager.Instance.GetEquidItemsFromDB()[i].prefabImage;
                 ItemUI.GetComponent<ButtonUI>().SetEquidmentsSO(ItemsManager.Instance.GetEquidItemsFromDB()[i]);
+                ItemUI.GetComponent<ButtonUI>().SetPriceTextForItem();
                 ItemUI.SetActive(true);
             }
             timeSpawnEquidmentButton--;
