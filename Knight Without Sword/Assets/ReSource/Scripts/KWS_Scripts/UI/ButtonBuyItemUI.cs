f5eed1e94be371203dba98236de1e76f9087ea7d
toan0812 <80931103+toan0812@users.noreply.github.com>
2023-11-08 14:38:06 +0700

    add new script

---
+++ Knight Without Sword/Assets/ReSource/Scripts/KWS_Scripts/UI/ButtonBuyItemUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBuyItemUI : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
